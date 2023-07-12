2fe0f3bbdeb3f32570e12438374305ace50cff74
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-07-12 16:18:16 -0400

    Update doc_generator.py

--- doc_generator.py
+++ doc_generator.py
@@ -1,13 +1,52 @@
+import os
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import OpenAI
 from config import model_name
 from dotenv import load_dotenv
 
-def generate_documentation(local_dir):
-    load_dotenv()
-    llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2)
+load_dotenv()
+llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2)
 
-    # This is where you would implement the function to generate the documentation.
-    # The implementation of this function depends on your exact requirements and the structure of your code.
-    # For example, you could iterate through the files in the repository, call a function to generate
-    # the documentation for each file, and write the documentation to a .md file in the local directory.
+filePrompt = """
+Write a detailed technical explanation of what this code does. 
+Focus on the high-level purpose of the code and how it may be used in the larger project.
+Include code examples where appropriate. Keep you response between 100 and 300 words. 
+DO NOT RETURN MORE THAN 300 WORDS.
+Output should be in markdown format.
+Do not just list the methods and classes in this file.
+"""
+
+folderPrompt = """
+Write a technical explanation of what the code in this file does
+and how it might fit into the larger project or work with other parts of the project.
+Give examples of how this code might be used. Include code examples where appropriate.
+Be concise. Include any information that may be relevant to a developer who is curious about this code.
+Keep you response under 400 words. Output should be in markdown format.
+Do not just list the files and folders in this folder.
+"""
+
+chatPrompt = ""
+
+def generate_file_documentation(file_path):
+    with open(file_path, 'r') as file:
+        code = file.read()
+    documentation = llm.ask_question(PromptTemplate(filePrompt), code)
+    return documentation
+
+def generate_folder_documentation(folder_path):
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    documentation = llm.ask_question(PromptTemplate(folderPrompt), ' '.join(files))
+    return documentation
+
+def generate_documentation(local_dir):
+    for root, dirs, files in os.walk(local_dir):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                documentation = generate_file_documentation(file_path)
+                with open(file_path + '.md', 'w') as doc_file:
+                    doc_file.write(documentation)
+        if dirs:
+            documentation = generate_folder_documentation(root)
+            with open(root + '/README.md', 'w') as doc_file:
+                doc_file.write(documentation)
